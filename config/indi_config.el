8598a024a4db11a158b423d81c63a3f7bbe7709b
Christoph Paulik <cpaulik@gmail.com>
2014-09-01 18:49:37 +0200

    added expand-region

--- config/indi_config.el
+++ config/indi_config.el
@@ -229,6 +229,11 @@
 (require 'workgroups2)
 ;;(workgroups-mode 1)
 
+;; expand region
+(require-package 'expand-region)
+(require 'expand-region)
+(global-set-key (kbd "C-=") 'er/expand-region)
+
 ;; helm package
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; PACKAGE: helm              ;;
