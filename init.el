0f47d24eb074b57dc149a061897366c538f01720
cp <cpaulik@gmail.com>
2014-09-06 23:08:09 +0200

    added a lot of org mode functionality from organize your life in plain text

--- init.el
+++ init.el
@@ -14,7 +14,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default)))
- '(org-agenda-files (quote ("~/Dropbox/Arbeit/organisation/projects/SGRT.org" "~/Dropbox/Arbeit/organisation/projects/GIO_GL.org" "~/Dropbox/Arbeit/organisation/projects/Univ_Ass.org" "~/Dropbox/Arbeit/science/2014/Correlation_Uncertainty.org" "~/organisation/notes.org" "~/organisation/work_tasks.org"))))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
