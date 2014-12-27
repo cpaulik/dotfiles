7833f1082dc69660ec2a583626ace0ca4063a156
Christoph Paulik <cpaulik@gmail.com>
2014-12-27 19:13:47 +0100

    fixed issue by loading parsebib manually

--- config/research_config.el
+++ config/research_config.el
@@ -1,9 +1,13 @@
 
 ;;(require-package 'ebib)
 ;;(require 'ebib)
+(load "parsebib.el")
 (autoload 'helm-bibtex "helm-bibtex" "" t)
 (setq helm-bibtex-bibliography "/home/cp/Dropbox/Arbeit/Papers/bibliography.bib")
-;;(setq helm-bibtex-library-path "~/Dropbox/Arbeit/Papers/pdf")
-;;(setq helm-bibtex-notes-path "~/Dropbox/Arbeit/Papers/notes")
-;;(setq helm-bibtex-notes-extension ".org")
+(setq helm-bibtex-library-path "~/Dropbox/Arbeit/Papers/pdf")
+(setq helm-bibtex-notes-path "~/Dropbox/Arbeit/Papers/notes")
+(setq helm-bibtex-notes-extension ".org")
+(setq helm-bibtex-pdf-open-function
+  (lambda (fpath)
+    (start-process "evince" "*evince*" "evince" fpath)))
 (provide 'research_config)
