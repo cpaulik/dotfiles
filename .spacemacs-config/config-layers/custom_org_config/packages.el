3a68beb5eb19bb3017812f68c2e9aa4cc85cdb11
Christoph Paulik <cpaulik@gmail.com>
2015-02-08 23:56:14 +0100

    removed org mobile features - trying out orgzly

--- .spacemacs-config/config-layers/custom_org_config/packages.el
+++ .spacemacs-config/config-layers/custom_org_config/packages.el
@@ -42,22 +42,11 @@ which require an initialization must be listed explicitly in the list.")
 (setq org-default-notes-file "~/Dropbox/org/refile.org")
 
 
-(setq org-mobile-inbox-for-pull "~/Dropbox/org/mobileinbox.org")
 (setq org-directory "~/Dropbox/org")
-(setq org-mobile-directory "~/Dropbox/MobileOrg")
 (setq org-agenda-files (quote ("~/Dropbox/org"
                                "~/Dropbox/Arbeit/organisation"
                                "~/Dropbox/Arbeit/organisation/projects")))
-;; push changes to mobile org automatically
-(defvar org-mobile-push-timer nil
-  "Timer that `org-mobile-push-timer' used to reschedule itself, or nil.")
 
-(defun org-mobile-push-with-delay (secs)
-  (when org-mobile-push-timer
-    (cancel-timer org-mobile-push-timer))
-  (setq org-mobile-push-timer
-        (run-with-idle-timer
-         (* 1 secs) nil 'org-mobile-push)))
 
 (evil-leader/set-key-for-mode 'org-mode
         "m'" 'org-edit-special)
@@ -71,25 +60,10 @@ which require an initialization must be listed explicitly in the list.")
 
 (evil-leader/set-key "oI" 'bh/punch-in)
 (evil-leader/set-key "oO" 'bh/punch-out)
-(add-hook 'after-save-hook
- (lambda ()
-   (when (eq major-mode 'org-mode)
-     (dolist (file (org-mobile-files-alist))
-      (if (string= (file-truename (expand-file-name (car file)))
-		   (file-truename (buffer-file-name)))
-           (org-mobile-push-with-delay 30)))
-   )))
-
-(run-at-time "00:05" 86400 '(lambda () (org-mobile-push-with-delay 1))) ;; refreshes agenda file each day
-(add-hook 'kill-emacs-hook 'org-mobile-push)
-
-;; fetch changes from mobile org
-(org-mobile-pull) ;; run org-mobile-pull at startup
 
 
 ;; Do a pull every 5 minutes to circumvent problems with timestamping
 ;; (ie. dropbox bugs)
-(run-with-timer 0 (* 5 60) 'org-mobile-pull)
 
 
 
