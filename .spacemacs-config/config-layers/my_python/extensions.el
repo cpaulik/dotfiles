c9e4b5649b92f4887d9f5c6538577ad877210b5d
Christoph Paulik <cpaulik@gmail.com>
2015-03-09 21:43:10 +0100

    fixed bug in python compile when exporting in org

--- .spacemacs-config/config-layers/my_python/extensions.el
+++ .spacemacs-config/config-layers/my_python/extensions.el
@@ -25,21 +25,26 @@
 
 ;; For each extension, define a function my_python/init-<extension-my_python>
 ;;
-(defun my_python/init-python-compile ()
-;;   "Initialize my extension"
-  (add-hook 'python-mode-hook
-          (lambda ()
-            (set (make-local-variable 'compile-command)
-                 (format "python %s" (file-name-nondirectory buffer-file-name)))))
-(defadvice compile (before ad-compile-smart activate)
-  "Advises `compile' so it sets the argument COMINT to t
-if breakpoints are present in `python-mode' files"
-  (when (derived-mode-p major-mode 'python-mode)
-    (save-excursion
-      (save-match-data
-        (goto-char (point-min))
-            ;; set COMINT argument to `t'.
-            (ad-set-arg 1 t)))))
+ (defun my_python/init-python-compile ()
+   "Initialize my extension"
+   ;; set compile command to buffer-file-name
+   ;; if buffer-file-name exists
+   ;; otherwise error occurs on e.g. org export including python src
+   (add-hook 'python-mode-hook
+           (lambda ()
+             (set (make-local-variable 'compile-command)
+                  (if buffer-file-name
+                      (format "python %s" (file-name-nondirectory buffer-file-name))))))
+
+    (defadvice compile (before ad-compile-smart activate)
+    "Advises `compile' so it sets the argument COMINT to t
+    if breakpoints are present in `python-mode' files"
+    (when (derived-mode-p major-mode 'python-mode)
+        (save-excursion
+        (save-match-data
+            (goto-char (point-min))
+                ;; set COMINT argument to `t'.
+                (ad-set-arg 1 t)))))
 )
 ;;
 ;; Often the body of an initialize function uses `use-package'
