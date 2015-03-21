d8eff062c0583574ec640da4f1ec8c04d4d095fd
Christoph Paulik <cpaulik@gmail.com>
2015-03-21 16:38:36 +0100

    improved powerline colors

--- .spacemacs-config/material-theme.el
+++ .spacemacs-config/material-theme.el
@@ -456,9 +456,9 @@
 
    ;; Powerline
    `(powerline-active1 ((t (:foreground ,foreground :background ,selection))))
-   `(powerline-active2 ((t (:foreground ,foreground :background ,current-line))))
-   `(powerline-inactive1 ((t (:foreground ,comment :background ,"#e0e0e0"))))
-   `(powerline-inactive2 ((t (:foreground ,comment :background ,"#9e9e9e"))))
+   `(powerline-active2 ((t (:foreground ,foreground :background ,"#78909c"))))
+   `(powerline-inactive1 ((t (:foreground ,comment :background ,selection))))
+   `(powerline-inactive2 ((t (:foreground ,comment :background ,selection))))
 
    ;; Outline
    `(outline-1 ((,class (:inherit nil
