344f927a771a7561b3e4b00a611ce40b698c3b05
Christoph Paulik <cpaulik@gmail.com>
2015-08-20 01:22:53 +0200

    awesome: set urxvt tmux as default terminal

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -62,7 +62,7 @@ beautiful.init(os.getenv("HOME") .. "/.config/awesome/themes/multicolor/theme.lu
 -- common
 modkey     = "Mod4"
 altkey     = "Mod1"
-terminal   = "gnome-terminal"
+terminal   = "urxvt -e tmux"
 editor     = os.getenv("EDITOR") or "nano" or "vi"
 editor_cmd = terminal .. " -e " .. editor
 
