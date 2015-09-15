deb6fb750275e2646c6a92fdc915c1a464c4f54c
Christoph Paulik <cpaulik@gmail.com>
2015-09-16 01:43:47 +0200

    mu4e add helm navigation and CGLS shortcut

--- .spacemacs-config/config-layers/my-mu4e/extensions.el
+++ .spacemacs-config/config-layers/my-mu4e/extensions.el
@@ -142,6 +142,7 @@
             ("/Personal/[Gmail].Trash"       . ?t)
             ("/Personal/[Gmail].All Mail"    . ?a)
             ("/TU/INBOX"       . ?w)
+            ("/TU/CGLS"       . ?C)
             ))
 
         ;; allow for updating mail using 'U' in the main view:
@@ -267,6 +268,9 @@
         (require 'org)
         (org-add-link-type "mu4e" 'org-mu4e-open)
         (add-hook 'org-store-link-functions 'org-mu4e-store-link)
+
+        ;; use helm for navigation
+        (setq  mu4e-completing-read-function 'completing-read)
 
         ;; spacemacs stuff
         (spacemacs|evilify-map mu4e-main-mode-map
