f9dbb3a69910c0d90b71f30a8038f1ee59cadeac
Christoph Paulik <cpaulik@gmail.com>
2015-10-07 20:10:05 +0200

    Add helm-mu package to my-mu4e layer

---
+++ .spacemacs-config/config-layers/my-mu4e/packages.el
@@ -0,0 +1,28 @@
+;;; packages.el --- my-mu4e Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; List of all packages to install and/or initialize. Built-in packages
+;; which require an initialization must be listed explicitly in the list.
+(defvar my-mu4e-packages
+  '(
+    ;; package names go here
+    helm-mu
+    ))
+
+;; For each package, define a function my-mu4e/init-<package-name>
+;;
+(defun my-mu4e/init-helm-mu ()
+  (use-package helm-mu
+    :defer t
+    :commands helm-mu
+    :init (evil-leader/set-key "oM" 'helm-mu))
+  )
