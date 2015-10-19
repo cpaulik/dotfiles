59c3b6102563ce4b350711b697fd8fa1893b53d9
Christoph Paulik <cpaulik@gmail.com>
2015-10-19 21:38:17 +0200

    Add pdf-tools layer draft

---
+++ .spacemacs-config/config-layers/pdf-tools/extensions.el
@@ -0,0 +1,31 @@
+;;; extensions.el --- pdf-tools Layer extensions File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(setq pdf-tools-pre-extensions
+      '(
+        ;; pre extension names go here
+        ))
+
+(setq pdf-tools-post-extensions
+      '(
+        ;; post extension names go here
+        ))
+
+;; For each extension, define a function pdf-tools/init-<extension-name>
+;;
+;; (defun pdf-tools/init-my-extension ()
+;;   "Initialize my extension"
+;;   )
+;;
+;; Often the body of an initialize function uses `use-package'
+;; For more info on `use-package', see readme:
+;; https://github.com/jwiegley/use-package
