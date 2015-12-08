376d3fe8c573acccbe2b74e866f0250b20f0d718
Christoph Paulik <cpaulik@gmail.com>
2015-12-08 15:36:02 +0100

    Mu4e, Add goto CC binding

--- .spacemacs-config/config-layers/my-mu4e/extensions.el
+++ .spacemacs-config/config-layers/my-mu4e/extensions.el
@@ -367,6 +367,7 @@ PGP: 8CFC D7DF 2867 B2DC 749B  1B0A 6E3B A262 5186 A0AC")
           "t" 'message-goto-to
           "m" 'message-goto-body
           "b" 'message-goto-bcc
+          "c" 'message-goto-cc
           "s" 'message-goto-subject)
 
 
