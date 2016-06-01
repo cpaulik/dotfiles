84b71d862e5f2d8a876685b1269314ed69a1d575
Christoph Paulik <cpaulik@gmail.com>
2016-06-01 23:24:49 +0200

    Adapt custom layers to defconst packages list.

--- .spacemacs-config/config-layers/my-mu4e/packages.el
+++ .spacemacs-config/config-layers/my-mu4e/packages.el
@@ -12,7 +12,7 @@
 
 ;; List of all packages to install and/or initialize. Built-in packages
 ;; which require an initialization must be listed explicitly in the list.
-(defvar my-mu4e-packages
+(defconst my-mu4e-packages
   '(
     ;; package names go here
     helm-mu
