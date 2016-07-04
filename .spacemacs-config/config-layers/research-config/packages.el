8296776930fc726a02396a00493330e9187b8546
Christoph Paulik <christoph.paulik@geo.tuwien.ac.at>
2016-07-04 12:02:31 +0200

    Spacemacs: update layers and research config.

--- .spacemacs-config/config-layers/research-config/packages.el
+++ .spacemacs-config/config-layers/research-config/packages.el
@@ -41,20 +41,13 @@ which require an initialization must be listed explicitly in the list.")
      :config
      (progn
 
-   (setq helm-bibtex-bibliography "~/Dropbox/Arbeit/Papers/bibliography.bib")
+   (setq bibtex-completion-bibliography "~/Dropbox/Arbeit/Papers/bibliography.bib")
    (setq helm-bibtex-library-path "~/Dropbox/Arbeit/Papers/pdf")
    (setq helm-bibtex-notes-path "~/Dropbox/Arbeit/Papers/notes")
    (setq helm-bibtex-notes-extension ".org")
    (setq helm-bibtex-pdf-open-function
      (lambda (fpath)
        (start-process "evince" "*evince*" "evince" fpath)))
-   (setq helm-bibtex-format-citation-functions
-      (quote
-       ((org-mode . helm-bibtex-format-citation-cite)
-        (latex-mode . helm-bibtex-format-citation-cite)
-        (tex-mode . helm-bibtex-format-citation-cite)
-        (markdown-mode . helm-bibtex-format-citation-pandoc-citeproc)
-        (default . helm-bibtex-format-citation-default))))
 
    (setq helm-bibtex-additional-search-fields '(keywords journal))
 
