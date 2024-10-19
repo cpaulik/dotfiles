3ba803a82df543bde2685d1fa67eaff977ee2875
Christoph Paulik <cpaulik@gmail.com>
2024-10-19 22:49:26 +0200

    Add a few useful tools

--- .spacemacs.d.symlink/init.el
+++ .spacemacs.d.symlink/init.el
@@ -94,7 +94,8 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(gptel)
+   dotspacemacs-additional-packages '(gptel
+                                      gruvbox-theme)
    ;; A list of packages and/or extensions that will not be install and loaded.
    dotspacemacs-excluded-packages '(
                                     material-theme
