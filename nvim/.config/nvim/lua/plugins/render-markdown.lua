704ef285b62608fe3e1960b64660e0477cbcb137
Christoph Paulik <cpaulik@gmail.com>
2025-08-29 00:09:46 +0200

    Fix markdown rendering to not jump around. Update lazy lock

---
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      heading = {
+        position = "inline",
+        width = "block",
+        left_pad = 0,
+        right_pad = 4,
+      },
+      code = {
+        sign = false,
+        border = "thick",
+      },
+      pipe_table = { style = "normal" },
+    },
+  },
+}
